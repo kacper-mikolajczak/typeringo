56cc058122364f6464e3b25fec9ef2c00448b976
KacperM717 <kacpermikolajczak10@gmail.com>
2021-01-29 13:31:19 +0100

    Themes fixed

--- static/scripts/themes.js
+++ static/scripts/themes.js
@@ -3,11 +3,10 @@ const theme = localStorage.getItem("theme");
 
 //REFERENCES
 const body = document.querySelector("body");
-body.classList.add(theme);
 const darkBtn = document.querySelector("#toggleDark");
 
 //EVENTS
 darkBtn.addEventListener("click", (e) => {
   body.classList.toggle("dark");
-  localStorage.setItem("theme", body.classList.contains("dark") ? "dark" : "");
+  localStorage.setItem("theme", body.classList.contains("dark") ? "dark" : "basic");
 });
