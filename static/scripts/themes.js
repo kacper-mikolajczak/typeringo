0141475043f570eadb394cc98f1713c9fc2ca534
KacperM717 <kacpermikolajczak10@gmail.com>
2021-01-29 13:02:03 +0100

    Theme preserved between pages

--- static/scripts/themes.js
+++ static/scripts/themes.js
@@ -1,8 +1,13 @@
+//Get preferences from localStorage
+const theme = localStorage.getItem("theme");
+
 //REFERENCES
 const body = document.querySelector("body");
+body.classList.add(theme);
 const darkBtn = document.querySelector("#toggleDark");
 
 //EVENTS
 darkBtn.addEventListener("click", (e) => {
   body.classList.toggle("dark");
+  localStorage.setItem("theme", body.classList.contains("dark") ? "dark" : "");
 });
